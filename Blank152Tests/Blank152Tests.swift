014c6383e526abf6fb2cff50079fcbda86779a8e
Алексей Бандура <alekseybandura@MacBook-Pro-Georgij.local>
2017-02-03 15:12:16 +0300

    Initial Commit

---
+++ Blank152Tests/Blank152Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Blank152Tests.swift
+//  Blank152Tests
+//
+//  Created by Алексей Бандура on 03.02.17.
+//  Copyright © 2017 Алексей Бандура. All rights reserved.
+//
+
+import XCTest
+@testable import Blank152
+
+class Blank152Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
